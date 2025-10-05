6d324ce828f396ac243253c5dcd30cb485addc52
mmani4590 <manikandan451990@gmail.com>
2025-10-04 22:51:54 -0400

    Create Project1.py

---
+++ Project1.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+daily_sales=np.array([255,275,385,210,390])
+
+print('average sales;',np.mean(daily_sales))
+print('maximum sales:',np.max(daily_sales))
+print('minimum sales:',np.min(daily_sales))
+
+import pandas as pd
+
+days=('Mon','Tue','Wed','Thu','Fri')
+
+sales_df=pd.DataFrame({'day':days,'sales':daily_sales})
+print(sales_df)
+
+sales_df['sales_With_ Tax']=sales_df['sales']*1.10
+print(sales_df)
+
+high_sales = sales_df[sales_df['sales'] > 300]
+print(high_sales)
+ 
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
